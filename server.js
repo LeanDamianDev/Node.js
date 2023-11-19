66fb25192de1e182238b0654456be97b24f74120
LeanDamianDev <125818512+LeanDamianDev@users.noreply.github.com>
2023-11-19 11:33:30 -0300

    Update server.js

--- server.js
+++ server.js
@@ -8,12 +8,9 @@ const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><head><style>body{background-color:blue}</style></head><body><h1>hola</h1></body></html>');
-
-
 }
 ); 
 server.listen(port,hostname, () => {
     console.log('server running on localhost', port);
-
 }
 );
